cd20221dddcdac8a5f7b733170385f353296ff19
indigo0086 <indigo0086@gmail.com>
2013-09-16 11:41:00 -0400

    * Added bower dependencies *Updated to use routing:

---
+++ web/js/app.js
@@ -0,0 +1,9 @@
+angular.module("spaLunchAndLearn", [])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: "views/main.html",
+                controller: "MainController"
+            })
+            .otherwise({ redirectTo: '/' });
+    });
---
+++ web/js/controllers/main.js
@@ -0,0 +1,6 @@
+angular.module("spaLunchAndLearn")
+    .controller("MainController", function($scope) {
+        $scope.groupCount = 0;
+        $scope.placesCount = 0;
+        $scope.upcomingEventsCount = 0;
+    });
