478724cd07e996a49051ea03a46d1a8fbe54db4a
indigo0086 <indigo0086@gmail.com>
2013-09-16 13:36:10 -0400

    * Seperating nav to seperate component * Updated controller naming scheme

---
+++ web/js/controllers/navController.js
@@ -0,0 +1,9 @@
+angular.module("spaLunchAndLearn")
+    .controller("NavController", function($scope) {
+        $scope.brandName = "Lunch-N-Learn";
+        $scope.menuItems = [
+            { title: "Groups", url: "/Groups" },
+            { title: "Events", url: "/Events" },
+            { title: "Places", url: "/Places" }
+        ]
+    });
