bf3761b32afbc393d99561df099572eb22817607
indigo0086 <indigo0086@gmail.com>
2013-10-03 15:40:32 -0400

    Adding Web service host project and updated models

---
+++ LunchAndLearnService/LunchAndLearnService/Controllers/LunchAndLearnController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using AttributeRouting.Web.Http;
+using LunchAndLearnService.Data;
+using LunchAndLearnService.Models;
+using Newtonsoft.Json;
+
+namespace LunchAndLearnService.Controllers
+{
+    public class LunchAndLearnController : ApiController
+    {
+        private static readonly LunchAndLearnRepository _repository;
+
+        static LunchAndLearnController()
+        {
+            _repository = new LunchAndLearnRepository();
+        }
+
+        #region User Methods
+
+        [GET("Users")]
+        public IEnumerable<User> GetAllUsers()
+        {
+            return _repository.Users;
+        }
+
+        [GET("Users/{id}")]
+        public User GetUserById(int id)
+        {
+            return _repository.Users.Single(u => u.UserId == id);
+        }
+
+        #endregion
+
+        #region Groups Methods
+
+        [GET("Groups")]
+        public IEnumerable<Group> GetAllGroups()
+        {
+            return _repository.Groups;
+        }
+
+        [GET("Groups/{id}")]
+        public Group GetGroupById(int id)
+        {
+            var groupById = _repository.Groups.Single(g => g.GroupId == id);
+            return groupById;
+        }
+
+        [GET("Groups/{id}/Users")]
+        public IEnumerable<User> GetUsersForGroupById(int id)
+        {
+            var grp = _repository.Groups.Single(g => g.GroupId == id);
+            if (grp != null)
+            {
+                return from userId in grp.MemberIds
+                       join user in _repository.Users
+                       on userId equals user.UserId
+                       select user;
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ LunchAndLearnService/LunchAndLearnService/Models/Models.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace LunchAndLearnService.Models
+{
+    public class Group
+    {
+        public int GroupId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<int> MemberIds { get; set; }
+        public ICollection<int> EventIds { get; set; }
+    }
+
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Suggestion> Suggestions { get; set; }
+    }
+
+    public class Suggestion
+    {
+        public int SuggestionId { get; set; }
+
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public int PlaceId { get; set; }
+        public virtual Place Place { get; set; }
+    }
+
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class Place
+    {
+        public int PlaceId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+    }
+}
