c9f1d07e9ba54b6670b00c1d9b35dc25205a8ed0
indigo0086 <indigo0086@gmail.com>
2013-10-07 17:05:45 -0400

    Added Username to page

--- web/js/App.js
+++ web/js/App.js
@@ -1,20 +1,25 @@
 angular.module("SpaApp.Services", ["ngResource"]);
 angular.module("SpaApp.Controllers", ["SpaApp.Services"]);
 
-angular.module("SpaApp", ["ngRoute", "SpaApp.Controllers"])
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when("/", {
-                templateUrl: "views/main.html",
-                controller: "MainController"
-            })
-            .when("/Groups", {
-                templateUrl: "views/groups.html",
-                controller: "GroupsController"
-            })
-            .when("/Groups/:groupId", {
-                templateUrl: "views/groupdetail.html",
-                controller: "GroupDetailController"
-            })
-            .otherwise({ redirectTo: "/" });
-    });
+var app = angular.module("SpaApp", ["ngRoute", "SpaApp.Controllers"]);
+
+//constants for username
+app.constant("userId", "1");
+app.constant("userName", "MonsterMash");
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "views/main.html",
+            controller: "MainController"
+        })
+        .when("/Groups", {
+            templateUrl: "views/groups.html",
+            controller: "GroupsController"
+        })
+        .when("/Groups/:groupId", {
+            templateUrl: "views/groupdetail.html",
+            controller: "GroupDetailController"
+        })
+        .otherwise({ redirectTo: "/" });
+});
--- web/js/controllers/NavController.js
+++ web/js/controllers/NavController.js
@@ -1,11 +1,11 @@
 angular.module("SpaApp.Controllers")
-    .controller("NavController", function ($scope) {
+    .controller("NavController", ["$scope", "userName", function ($scope, userName) {
         $scope.isCollapsed = true;
-
+        $scope.userName = userName;
         $scope.brandName = "Lunch-N-Learn";
         $scope.menuItems = [
             { title: "Groups", url: "#/Groups" },
             { title: "Events", url: "#/Events" },
             { title: "Places", url: "#/Places" }
         ]
-    });
+    }]);
